310436b82564a118a1ac97ed7a38cb70fbfd0d2a
Dilantha <dilantha.prasanjith111@gmail.com>
2023-05-15 17:17:38 +0800

    update e2e test so that it will not make an actual http call

--- packages/backend/test/app.e2e-spec.ts
+++ packages/backend/test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { ForeCastItem } from '../src/types/ForeCastItem.type';
+import * as nock from 'nock';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -22,6 +23,25 @@ describe('AppController (e2e)', () => {
 
   it('/forecast (GET) should return an array of ForeCastItems', async () => {
     const query = { date_time: '2023-04-29T10:00:00' };
+
+    // Mock the API response using nock
+    nock('https://api.example.com')
+      .get('/weather')
+      .query(query)
+      .reply(200, {
+        forecast: [
+          {
+            locationName: 'New York City',
+            screenShotUrl: 'https://example.com/screenshot.png',
+            whetherForecast: 'Cloudy',
+          },
+          {
+            locationName: 'San Francisco',
+            screenShotUrl: 'https://example.com/screenshot.png',
+            whetherForecast: 'Sunny',
+          },
+        ],
+      });
 
     const response = await request(app.getHttpServer())
       .get('/forecast')
