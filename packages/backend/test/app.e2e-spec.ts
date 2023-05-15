2f06e897b1ec33cf15ec33e05e239598b997b2bb
Dilantha <dilantha.prasanjith111@gmail.com>
2023-05-15 17:03:00 +0800

    update e2e test backend

--- packages/backend/test/app.e2e-spec.ts
+++ packages/backend/test/app.e2e-spec.ts
@@ -1,13 +1,14 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
-import { INestApplication } from '@nestjs/common';
+import { ForeCastItem } from '../src/types/ForeCastItem.type';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
@@ -15,14 +16,24 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await app.close();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/forecast (GET) should return an array of ForeCastItems', async () => {
+    const query = { date_time: '2023-04-29T10:00:00' };
+
+    const response = await request(app.getHttpServer())
+      .get('/forecast')
+      .query(query)
+      .expect(HttpStatus.OK);
+
+    expect(response.body.forecast).toBeInstanceOf(Array);
+
+    response.body.forecast.forEach((forecastItem: ForeCastItem) => {
+      expect(forecastItem).toHaveProperty('locationName');
+      expect(forecastItem).toHaveProperty('screenShotUrl');
+      expect(forecastItem).toHaveProperty('whetherForecast');
+    });
   });
 });
